451ba5cfdb331fc54e267bed53a803b5bfdcef38
Ohta Takuma <no.more.ria10@gmail.com>
2013-07-02 20:53:07 +0900

    add motion_flag

--- motion/motion.cpp
+++ motion/motion.cpp
@@ -5,14 +5,20 @@
 
 #include<iostream>
 #include<math.h>
+#include<stdbool.h>
 #include"object_struct.hpp"
 #include"calc.hpp"
 #include"motion.hpp"
 #include"time_set.hpp"
 
+static bool f_flag = false; //false: slope, true: free
+
+
 void free_fall(Ball* ball){
     double dv[3];
     double dp[3];
+    if( !f_flag ) f_flag = true;
+    
     dv[0] = 0;
     dv[1] = - G * DT;
     dv[2] = 0;
@@ -31,6 +37,10 @@ void slope_fall(Ball* ball, Plane* plane){
     double dv[3];
     double dp[3];    
     double abs_dv = G * DT; //坂を転がる速度の変化の絶対値
+    if( f_flag ){
+        f_flag = false;
+        ball->vel[1] = 0;
+    }
     dv[0] = abs_dv * cos(plane->theta) * plane->nomal_vec[0] ;
     dv[1] = abs_dv * sin(plane->theta) * sin(plane->theta) ;
     dv[2] = abs_dv * cos(plane->theta) * plane->nomal_vec[2] ;
