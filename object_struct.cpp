c261dda6f9e28eec09bb5554fb52149c4cce2f3f
Ohta Takuma <no.more.ria10@gmail.com>
2013-07-02 13:08:11 +0900

    add object_struct

--- object_struct.cpp
+++ object_struct.cpp
@@ -2,24 +2,34 @@
 #include<iostream>
 #include"object_struct.hpp"
 /*平面の計算のメンバ変数。*/
+const double Plane::m_pl_init[3][3]  = { {INIT_X,0, INIT_Z}, {-1 * INIT_X, 0, INIT_Z}, { INIT_X, 0, -1 * INIT_Z} }; //初期の平面の頂点。静的メンバ
+
 
 void Plane::CalcABCD(){
     int p[3][3]; //回転後の頂点の格納。
-    //頂点の回転後座標の計算。ｚ軸周りがalpha,x軸周りがbeta
+    //頂点の回転後座標の計算。ｚ軸周りがm_pl_alpha,x軸周りがbeta
     for(int i = 0; i < 3 ; i++){   
-        p[i][0] = init[i][0] * cos(alpha) - init[i][1] * sin(alpha) * cos(beta) + init[i][2] * sin(alpha) * sin(beta);
-        p[i][1] = init[i][0] * sin(alpha) + init[i][1] * cos(alpha) * cos(beta) - init[i][2] * cos(alpha) * sin(beta);
-        p[i][2] = init[i][0] * 0.0 + init[i][1] * sin(beta) - init[i][2] * cos(beta);
+        p[i][0] = m_pl_init[i][0] * cos(m_pl_alpha) - m_pl_init[i][1] * sin(m_pl_alpha) * cos(m_pl_beta ) + m_pl_init[i][2] * sin(m_pl_alpha) * sin(m_pl_beta );
+        p[i][1] = m_pl_init[i][0] * sin(m_pl_alpha) + m_pl_init[i][1] * cos(m_pl_alpha) * cos(m_pl_beta ) - m_pl_init[i][2] * cos(m_pl_alpha) * sin(m_pl_beta );
+        p[i][2] = m_pl_init[i][0] * 0.0 + m_pl_init[i][1] * sin(m_pl_beta ) - m_pl_init[i][2] * cos(m_pl_beta );
     }
     //a,b,c,dの計算。dの計算にはa,b,cを用いることに注意
     m_pl_a = ( p[1][1] - p[0][1] ) * ( p[2][2] - p[0][2] ) - (p[2][1] - p[0][1] ) * (p[1][2] - p[0][2] );
     m_pl_b = ( p[1][2] - p[0][2] ) * ( p[2][0] - p[0][0] ) - (p[2][2] - p[0][2] ) * (p[1][0] - p[0][0] ) ;
     m_pl_c = ( p[1][0] - p[0][0] ) * ( p[2][1] - p[0][1] ) - (p[2][0] - p[0][0] ) * (p[1][1] - p[0][1] ); 
     m_pl_d = -p[0][0] * m_pl_a -p[0][1] * m_pl_b -p[0][0] * m_pl_c;
+    return ;
 }
 
 //初期化。初期の点は固定。その他の値は計算して出します。
-void Plane::Plane(): init{ {INIT_X,0, INIT_Z}, {-1 * INIT_X, 0, INIT_Z}, { INIT_X, 0, -1 * INIT_Z} } {
-        alpha = beta = a = b = c = d =  0.0;
-        CalcABCD();
-}
+Plane::Plane() 
+ {
+        this->m_pl_alpha = m_pl_beta  = m_pl_a = m_pl_b = m_pl_c = m_pl_d =  0.0;
+        this->CalcABCD();
+ }
+
+void Plane::SetAlphaBeta ( double alpha2, double beta2){
+    if(alpha2 != INF) this->m_pl_alpha = alpha2;
+    if(alpha2 != INF) this->m_pl_beta = beta2;
+    return;
+}    
--- object_struct.hpp
+++ object_struct.hpp
@@ -2,35 +2,27 @@
 
 
 #include<iostream>
-#define INF 1000000.0lf
+#define INF 1000000.0f
 //初期平面の頂点座標のx,z方向長さの半分の値。
-
-static const int INIT_X = 50;
-static const int INIT_Z = 50;
+#define INIT_X  50.0f
+#define INIT_Z  50.0f
     
 //平面の情報のオブジェクト
-private class Plane{
+class Plane{
+private:
     double m_pl_alpha; //z軸周りの回転角
     double m_pl_beta;//x軸周りの回転角
     double m_pl_a; //以下係数
     double m_pl_b;
     double m_pl_c;
     double m_pl_d;
-    static const double  init[3][3]; 初期の平面の計算.
+    static const double  m_pl_init[3][3] ; //初期の平面の頂点。静的メンバ
 public:
-    //alphaとbetaから平面の方程式のa, b, c, dを計算し、メンバ変数に格納する。
+    //alphaとbetaから平面の方程式のa, b, c, dを計算し、メンバ変数に格納する。    
     void CalcABCD();
     //角度をセットする。丸め込みをしてから書き込むこと。INF代入された場合は何もしない。
-    void SetAlphaBeta( double alpha2, double beta2){
-        if(alpha2 != INF) this:alpha = alpha2;
-        if(alpha2 != INF) this:beta =　beta2;
-    };
+    void SetAlphaBeta(double , double);
+    Plane();
 
-    
-    
-    };
-//初期化。初期の点は固定。その他の値は計算して出します。
-void Plane(): init{ {INIT_X,0, INIT_Z}, {-1 * INIT_X, 0, INIT_Z}, { INIT_X, 0, -1 * INIT_Z} {
-        alpha = beta = a = b = c = d =  0.0;
-           
         
+};
